122b4f7ed3e53917949b59819d6fa7789dd17ab6
riikaseeba <riika.seeba@ut.ee>
2024-11-21 16:52:52 +0200

    tring different things.

--- al_group/src/views/HomeView.vue
+++ al_group/src/views/HomeView.vue
@@ -1,24 +1,18 @@
 <template>
-  <div>
-      <h1>Postitused!</h1>
-    <main>
-      <BlogPost  />
-    </main>
-
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-
-import BlogPost from '@/components/BlogPost.vue'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  name: 'HomeView',
   components: {
-    BlogPost
+    HelloWorld
   }
 }
 </script>
-
-<style scoped>
-/* Your home page styles here */
-</style>
